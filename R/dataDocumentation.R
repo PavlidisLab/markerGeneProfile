be1e7a560b1ff7eff52b5bc384cad825a48a1ec0
Ogan Mancarci <ogan.mancarcii@gmail.com>
2016-12-15 20:55:18 -0800

    mouseMarkerGenes added

--- R/dataDocumentation.R
+++ R/dataDocumentation.R
@@ -1,1 +1,7 @@
+#' Brain cell type specific genes
+#'
+#' A list of brain cell type specific genes selected from mouse pooled cell type microarray data.
+#' Published in Mancarci et al. 2016.
+'mouseMarkerGenes'
+
 
---
+++ process/process.R
@@ -0,0 +1,5 @@
+# add marker genes from analysis package and create sample data
+
+loadURL("https://github.com/oganm/neuroExpressoAnalysis/blob/master/data/mouseMarkerGenes.rda?raw=true")
+use_data(mouseMarkerGenes)
+
